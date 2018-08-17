62c3b79fec0c20c07b780fdcb2400f3631a650d4
Chang Sophia <you@example.com>
2018-08-17 16:03:58 +0800

    initial

--- SpidermanSliderCloser/ViewController.swift
+++ SpidermanSliderCloser/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var spidermanImageView: UIImageView!
+    @IBOutlet weak var deadpoolImageView: UIImageView!
+    
+    @IBAction func fightSlider(_ sender: UISlider) {
+        spidermanImageView.frame.origin.x = CGFloat(158-50*sender.value)
+        spidermanImageView.frame.origin.y = CGFloat(0+200*sender.value)
+         deadpoolImageView.frame.origin.x = CGFloat(0+50*sender.value)
+        deadpoolImageView.frame.origin.y = CGFloat(445-100*sender.value)
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
